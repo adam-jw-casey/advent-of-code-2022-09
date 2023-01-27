284609dc50cafa6f7c256cbad51152703719bc06
Adam Casey <adam.jw.casey@gmail.com>
2023-01-26 21:46:54 -0700

    Solves part one

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 use std::ops;
 use std::cmp::PartialEq;
 #[macro_use] extern crate impl_ops;
+use sscanf::sscanf;
+use std::collections::HashSet;
 
 pub enum Direction{
     Up,
@@ -20,12 +22,10 @@ impl Direction{
     }
 }
 
-#[derive(Copy, Clone)]
-#[derive(Debug)]
-#[derive(PartialEq)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
 pub struct Position{
-    pub x: i8,
-    pub y: i8
+    pub x: i32,
+    pub y: i32
 }
 
 impl Position{
@@ -77,35 +77,84 @@ impl Snake{
     /// use advent_of_code_2022_09::Position;
     /// let mut snake = Snake::new();
     ///
-    /// snake.step(Direction::Up);
+    /// snake.step(&Direction::Up);
     /// assert_eq!(snake.head, Position{x:0, y:1});
     /// assert_eq!(snake.tail, Position{x:0, y:0});
     ///
-    /// snake.step(Direction::Right);
+    /// snake.step(&Direction::Right);
     /// assert_eq!(snake.head, Position{x:1, y:1});
     /// assert_eq!(snake.tail, Position{x:0, y:0});
     ///
-    /// snake.step(Direction::Right);
+    /// snake.step(&Direction::Right);
     /// assert_eq!(snake.head, Position{x:2, y:1});
     /// assert_eq!(snake.tail, Position{x:1, y:1});
     ///
-    /// snake.step(Direction::Right);
+    /// snake.step(&Direction::Right);
     /// assert_eq!(snake.head, Position{x:3, y:1});
     /// assert_eq!(snake.tail, Position{x:2, y:1});
     ///
-    /// snake.step(Direction::Down);
+    /// snake.step(&Direction::Down);
     /// assert_eq!(snake.head, Position{x:3, y:0});
     /// assert_eq!(snake.tail, Position{x:2, y:1});
     ///
-    /// snake.step(Direction::Left);
+    /// snake.step(&Direction::Left);
     /// assert_eq!(snake.head, Position{x:2, y:0});
     /// assert_eq!(snake.tail, Position{x:2, y:1});
     /// ```
-    pub fn step(&mut self, dir: Direction){
+    pub fn step(&mut self, dir: &Direction){
         self.head = &self.head + dir.delta();
         let trail = &self.head - &self.tail;
         if trail.x.abs() > 1 || trail.y.abs() > 1{
             self.tail = self.head - dir.delta();
         }
     }
+}
+
+/// Counts the number of unique positions the snake's tail occupies
+/// while following the commands in input_moves
+/// # Examples
+/// ```
+/// use advent_of_code_2022_09::count_tail_positions;
+/// assert_eq!(
+///     13,
+///     count_tail_positions(concat!(
+///         "R 4\n",
+///         "U 4\n",
+///         "L 3\n",
+///         "D 1\n",
+///         "R 4\n",
+///         "D 1\n",
+///         "L 5\n",
+///         "R 2"
+///     ))
+/// )
+/// ```
+pub fn count_tail_positions(input_moves: &str) -> usize{
+    let mut snake = Snake::new();
+    let mut tail_positions = HashSet::new();
+    let mut dir: Direction;
+    let mut n: u8;
+    let mut parsed: (char, u8);
+
+    tail_positions.insert(snake.tail);
+    for line in input_moves.lines(){
+        parsed = sscanf!(line, "{char} {u8}")
+            .expect("Input should be properly formatted");
+
+        dir = match parsed.0{
+            'U' => Direction::Up,
+            'D' => Direction::Down,
+            'L' => Direction::Left,
+            'R' => Direction::Right,
+            _   => panic!("Invalid input!")
+        };
+        n = parsed.1;
+
+        for _i in 0..n{
+            snake.step(&dir);
+            tail_positions.insert(snake.tail);
+        }
+    }
+
+    tail_positions.len()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use std::env;
 use std::fs;
-use advent_of_code_2022_09::Snake;
+use advent_of_code_2022_09::count_tail_positions;
 
 fn main() {
     let args = env::args().collect::<Vec<_>>();
     let file_path = &args[1];
     let contents = fs::read_to_string(file_path).expect("Should have been able to read {file_path}");
+
+    println!("The tail visited {} positions!", count_tail_positions(&contents));
 }
